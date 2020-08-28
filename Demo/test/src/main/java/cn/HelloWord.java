3d2110eb8115f0017cd04aef1035f6afde807b8f
lpy <lpy@itcast.cn>
2020-08-28 10:55:55 +0800

    添加文件

---
+++ Demo/test/src/main/java/cn/HelloWord.java
@@ -0,0 +1,12 @@
+package cn;
+
+/**
+ * @Auther: 梁培义
+ * @date 2020-08-28 10:48
+ * @Description:
+ */
+public class HelloWord {
+    public static void main(String[] args) {
+        System.out.println("helloword");
+    }
+}
