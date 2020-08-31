b11067fbdb8c830d2079788ac00dc377c3f380fb
lpy857 <337333891@qq.com>
2020-08-31 14:04:51 +0800

    创建本地仓库

---
+++ dome_01/testgit/src/main/java/helloword.java
@@ -0,0 +1,12 @@
+/**
+ * @Auther: 梁培义
+ * @date 2020-08-31 13:56
+ * @Description:
+ */
+public class helloword {
+    public static void main(String[] args) {
+
+        System.out.println("创建本地仓库");
+
+    }
+}
